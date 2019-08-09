2daf1b8247af015b0a0df4f8005aa04ad41a1738
Marcuz.Huang <marcuz.huang@gmail.com>
2019-08-09 15:15:57 +0800

    first commit

---
+++ driving.py
@@ -0,0 +1,19 @@
+# driving
+country = input('enter you country:')
+age = input('enter you age:')
+
+age=int(age)
+
+if country == '台灣':
+	if age >= 18 :
+		print('you are qualified to the exam')
+	else :
+		print('you are not qualified')
+
+elif country = '美國':
+	if age >= 15 :
+		print('you are qualified to the exam')
+	elif :
+		print('you are not qualified')
+else :
+	print('your country is not involved the list')
